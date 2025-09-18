8ef3512cd187f6a9e5e0b6713ad617e476e555bd
Thathshila <thathshilaashanganie9501@gmail.com>
2025-09-18 21:02:46 +0530

    add daily note

--- types/vibe.ts
+++ types/vibe.ts
@@ -2,6 +2,7 @@
 import { Timestamp } from "firebase/firestore";
 
 export interface DailyReadingVibe {
+  id: string; // Unique ID combining userId and date
   emoji: string;
   note: string;
   date: string; // Format: YYYY-MM-DD
